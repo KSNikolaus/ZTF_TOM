b62293ba1ec503f1258f0d857ac99c5d5a3a73dc
rachel3834 <rstreet@lco.global>
2019-06-20 20:42:57 -0700

    Reset the DB location path

--- ZTF_TOM/settings.py
+++ ZTF_TOM/settings.py
@@ -104,8 +104,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': os.path.join('/var/www/ztftom/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'NAME': os.path.join('/var/www/ztftom/db.sqlite3'),
     }
 }
 
